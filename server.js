11b4afc5c6ccfe9cf045ee602c2d6d050d3262d5
Glitch (lxdnz254-file-metadata) <none>
2019-02-24 02:13:12 +0000

    😹🎻 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer')
+var upload = multer().single('upfile')
 
 var app = express();
 
@@ -17,6 +19,12 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload,(req, res) => {
+  
+    // Everything went fine.
+    res.json({filename: req.file.originalname, size: req.file.size})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
